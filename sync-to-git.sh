58b6932ed7031da7979a8755f703cb52b1f3baaf
PK <pk@pknw1.co.uk>
2019-10-19 04:26:51 +0200

    Sat 19 Oct 04:26:51 CEST 2019

--- sync-to-git.sh
+++ sync-to-git.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
 
+	function logo(){
+		        echo " "
+
+
+			echo " ██████╗ ██╗████████╗██╗      █████╗ ██████╗ ███████╗██╗   ██╗███╗   ██╗ ██████╗";
+			echo "██╔════╝ ██║╚══██╔══╝██║     ██╔══██╗██╔══██╗██╔════╝╚██╗ ██╔╝████╗  ██║██╔════╝";
+			echo "██║  ███╗██║   ██║   ██║     ███████║██████╔╝███████╗ ╚████╔╝ ██╔██╗ ██║██║     ";
+			echo "██║   ██║██║   ██║   ██║     ██╔══██║██╔══██╗╚════██║  ╚██╔╝  ██║╚██╗██║██║     ";
+			echo "╚██████╔╝██║   ██║   ███████╗██║  ██║██████╔╝███████║   ██║   ██║ ╚████║╚██████╗";
+			echo " ╚═════╝ ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═════╝ ╚══════╝   ╚═╝   ╚═╝  ╚═══╝ ╚═════╝";
+			echo "                                                                                ";
+
+									}
+cd $(dirname $0)
+if ! [[ -d ./.git ]]; then clear; logo; echo "No Git Directory"; exit 99; fi
+
 if [[ -z $(git status | grep "nothing to commit") ]]; 
 then
-	echo commit
+	clear
+	logo
+	git add . || echo "Error Adding Files"
+	git commit -m "$(date)" || echo "Error Commiting Files"
+	git push || echo "Error Pushing Files"
+	echo " "
+	echo "Synced Updates"
+	exit 0
 else
+	logo
 	echo nothing to commit
+	exit 0
 fi
